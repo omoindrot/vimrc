f6fb4ce0d4e344a9719b312029c6f656d4a0d8a9
Olivier Moindrot <omoindrot@gmail.com>
2019-03-13 11:33:08 +0100

    rename to dotfiles, add zsh config

---
+++ dotfiles/custom.zsh
@@ -0,0 +1,76 @@
+# User configuration
+
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# ssh
+# export SSH_KEY_PATH="~/.ssh/rsa_id"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+
+# PURE CONFIG
+autoload -U promptinit; promptinit
+prompt pure
+
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/oliviermoindrot/workspace/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/oliviermoindrot/workspace/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/oliviermoindrot/workspace/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/oliviermoindrot/workspace/google-cloud-sdk/completion.zsh.inc'; fi
+
+
+# Change key timeout (the default is 40)
+export KEYTIMEOUT=20
+
+# VIM MODE IN ZSH
+# Add vim mode
+bindkey -v
+
+# Change to normal mode with "jk"
+bindkey -M viins "jk" vi-cmd-mode
+
+# Beginning search with arrow keys
+bindkey "^[OA" up-line-or-beginning-search
+bindkey "^[OB" down-line-or-beginning-search
+bindkey -M vicmd "^[OA" up-line-or-beginning-search
+bindkey -M vicmd "^[OB" down-line-or-beginning-search
+bindkey -M vicmd "k" up-line-or-beginning-search
+bindkey -M vicmd "j" down-line-or-beginning-search
+
+# Updates editor information when the keymap changes.
+function zle-keymap-select() {
+  zle reset-prompt
+  zle -R
+}
+
+zle -N zle-keymap-select
+
+function vi_mode_prompt_info() {
+  echo "${${KEYMAP/vicmd/[% NORMAL]%}/(main|viins)/[% INSERT]%}"
+}
+
+# Define right prompt, regardless of whether the theme defined it
+RPS1='$(vi_mode_prompt_info)'
+RPS2=$RPS1
--- install_my_config.sh
+++ install_my_config.sh
@@ -1,28 +1,31 @@
 # Install zsh and oh-my-zsh
 cd ~
 if ! [ -x "$(command -v zsh)" ]; then
-    echo 'Error: zsh is not installed.' >&2
-    echo 'Install it with `sudo apt-get install zsh`'
-    echo 'And install oh-my-zsh also'
-    echo 'With `sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"`'
+    echo "Error: zsh is not installed." >&2
+    echo "Install it with `sudo apt-get install zsh`"
+    echo "And install oh-my-zsh also"
+    echo "With `sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"`"
     exit 1
 fi
 
 cd ~/.vim_runtime
-cp ~/.vim_runtime/configs/tmux.conf ~/.tmux.conf
+cp ~/.vim_runtime/dotfiles/tmux.conf ~/.tmux.conf
 echo "Installed tmux configuration successfully! Enjoy :-)"
 
-cp ~/.vim_runtime/configs/gitconfig ~/.gitconfig
+cp ~/.vim_runtime/dotfiles/gitconfig ~/.gitconfig
 echo "Installed git configuration successfully! Enjoy :-)"
 
-cp ~/.vim_runtime/configs/bash_profile ~/.bash_profile
+cp ~/.vim_runtime/dotfiles/bash_profile ~/.bash_profile
 echo "Installed bash_profile"
 
-cp ~/.vim_runtime/configs/gitignore ~/.gitignore
+cp ~/.vim_runtime/dotfiles/gitignore ~/.gitignore
 echo "Installed user .gitignore"
 
-cp ~/.vim_runtime/configs/ipython_config.py ~/.ipython/profile_default/ipython_config.py
+cp ~/.vim_runtime/dotfiles/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 echo "Installed user ipython_config.py"
 
-cp ~/.vim_runtime/configs/custom.js ~/.jupyter/custom/custom.js
+cp ~/.vim_runtime/dotfiles/custom.js ~/.jupyter/custom/custom.js
 echo "Installed user jupyter config"
+
+cp ~/.vim_runtime/dotfiles/custom.zsh ~/.oh-my-zsh/custom/custom.zsh
+echo "Installed my zsh custom config"
