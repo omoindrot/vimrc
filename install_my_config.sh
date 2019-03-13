8082b9d5d817bd0313a7b13e1637870d87516558
Olivier Moindrot <omoindrot@gmail.com>
2019-03-13 13:26:23 +0100

    better behavior for pure install

--- install_my_config.sh
+++ install_my_config.sh
@@ -33,7 +33,13 @@ echo "Installed my zsh custom config"
 
 # Install pure prompt for zsh
 # Clone it in the .oh-my-zsh repo
-git clone https://github.com/sindresorhus/pure ~/.oh-my-zsh/custom/plugins/pure
+PURE_FOLDER="$HOME/.oh-my-zsh/custom/plugins/pure"
+if [ ! -d "$PURE_FOLDER" ] ; then
+    git clone https://github.com/sindresorhus/pure ~/.oh-my-zsh/custom/plugins/pure
+fi
 # Link the files to ~/.zfunctions (which is added in the $fpath)
+mkdir -p "$HOME/.zfunctions"
+# Give correct rights
+chmod 755 "$HOME/.zfunctions"
 ln -s "$HOME/.oh-my-zsh/custom/plugins/pure/pure.zsh" "$HOME/.zfunctions/prompt_pure_setup"
 ln -s "$HOME/.oh-my-zsh/custom/plugins/pure/async.zsh" "$HOME/.zfunctions/async"
