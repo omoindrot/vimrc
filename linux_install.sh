3834458b70d669c49ab05274c3987d4cef01ed14
Olivier Moindrot <omoindrot@gmail.com>
2019-10-23 13:49:19 +0000

    add linux install

---
+++ linux_install.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo add-apt-repository -y ppa:jonathonf/vim
+sudo apt update
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install -y \
+    software-properties-common \
+    vim \
+    python3.7 \
+    python3.7-dev \
+    python3.7-pip \
+    python3.7-venv \
+    openslide-tools \
+    python3-openslide \
+    zip \
+    unzip
